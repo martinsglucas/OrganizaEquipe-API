81c1752167cc2c7d40d35f12f9ac83db03fe3e5b
martinsglucas <martin5.luca5.univ@gmail.com>
2025-03-31 12:44:30 -0300

    feat: captura filtros de equipe
    #2

--- escala/views/equipe.py
+++ escala/views/equipe.py
@@ -7,9 +7,12 @@ class EquipeViewSet(ModelViewSet):
     def get_queryset(self):
         queryset = super().get_queryset()
         user_only = self.request.query_params.get('userOnly', 'false').lower() == 'true'
+        codigo_equipe = self.request.query_params.get('codeAccess', None)
         
         if user_only:
             queryset = queryset.filter(membros=self.request.user)
+        if codigo_equipe:
+            queryset = queryset.filter(codigo_de_acesso=codigo_equipe)
         
         return queryset
     def get_serializer_class(self):
