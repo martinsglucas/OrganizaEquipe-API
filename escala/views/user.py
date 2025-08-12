47fb2f28e57973a6fe92d06c35b2e3f9a7e0df12
martinsglucas <martin5.luca5.univ@gmail.com>
2025-08-12 15:58:04 -0300

    style: reescreve user
    #1

--- escala/views/user.py
+++ escala/views/user.py
@@ -1,10 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
-from escala.serializers import UsuarioSerializer
-from escala.models import Usuario
+from escala.models import User
+from escala.serializers import UserSerializer
 from escala.permissions import AllowPostWithoutAuthentication
 
-class UsuarioViewSet(ModelViewSet):
-    queryset = Usuario.objects.all()
-    serializer_class = UsuarioSerializer
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
     permission_classes = [AllowPostWithoutAuthentication]
     # http_method_names = ['get', 'post', 'put', 'delete']
