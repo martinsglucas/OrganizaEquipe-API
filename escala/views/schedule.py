71fb01871b4d6f8dff666be6ed669a1dba83b482
martinsglucas <martin5.luca5.univ@gmail.com>
2025-08-15 11:22:29 -0300

    feat: adiciona filtro de escalas
    #4

--- escala/views/schedule.py
+++ escala/views/schedule.py
@@ -4,6 +4,22 @@ from escala.serializers import CreateScheduleSerializer, RetrieveScheduleSeriali
 
 class ScheduleViewSet(ModelViewSet):
     queryset = Schedule.objects.all()
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        user_only = self.request.query_params.get('userOnly', 'false').lower() == 'true'
+
+        if user_only:
+            queryset = (
+                queryset.filter(
+                    participations__user=self.request.user
+                ) | queryset.filter(
+                    team__admins=self.request.user
+                )
+            ).distinct()
+
+        return queryset.order_by('date', 'hour')
+    
     def get_serializer_class(self):
         if self.action in ['create', 'update', 'partial_update', 'destroy']:
             return CreateScheduleSerializer
